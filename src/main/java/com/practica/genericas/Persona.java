4ac41c91907179f7710355f8d676d12353dd1069
rocio.tagarro <rocio.tagarro@alumnos.upm.es>
2024-04-25 16:14:50 +0200

    elimino getters en Persona #1

--- src/main/java/com/practica/genericas/Persona.java
+++ src/main/java/com/practica/genericas/Persona.java
@@ -20,49 +20,31 @@ public class Persona {
 		this.fechaNacimiento = fechaNacimiento;
 	}
 
-	public String getNombre() {
-		return nombre;
-	}
 
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
 
-	public String getApellidos() {
-		return apellidos;
-	}
 
 	public void setApellidos(String apellidos) {
 		this.apellidos = apellidos;
 	}
 
-	public String getDocumento() {
-		return documento;
-	}
 
 	public void setDocumento(String documento) {
 		this.documento = documento;
 	}
 
-	public String getEmail() {
-		return email;
-	}
 
 	public void setEmail(String email) {
 		this.email = email;
 	}
 
-	public String getDireccion() {
-		return direccion;
-	}
 
 	public void setDireccion(String direccion) {
 		this.direccion = direccion;
 	}
 
-	public String getCp() {
-		return cp;
-	}
 
 	public void setCp(String cp) {
 		this.cp = cp;
